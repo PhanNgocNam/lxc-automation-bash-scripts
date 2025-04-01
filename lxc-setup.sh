5b37705663dbc2ff299186c34d6afdedf55da533
PhanNgocNam <realcm471@gmail.com>
2025-04-01 18:11:33 +0700

    few tweak

--- lxc-setup.sh
+++ lxc-setup.sh
@@ -15,11 +15,9 @@ echo "Grant sudo permissions for $1 #########################"
 cd /etc/sudoers.d && touch $1 && echo "$1 ALL=(ALL) NOPASSWD:ALL" >> $1
 
 echo "Add ssh public key for $1 #############################"
-su - $1
-pwd
-mkdir -p ~/.ssh
-touch ~/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvHqRmVP5IX7wivyWC1SXlr/cEe3nq3fDDFNHpRrh1P p2n@PhanNgocNams-MacBook-Air.local" >> ~/.ssh/authorized_keys
+mkdir -p /home/$1/.ssh
+touch /home/$1/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvHqRmVP5IX7wivyWC1SXlr/cEe3nq3fDDFNHpRrh1P p2n@PhanNgocNams-MacBook-Air.local" >> /home/$1/.ssh/authorized_keys
 
 echo "Install net-tools #####################################"
 apt install -y net-tools >> /dev/null
