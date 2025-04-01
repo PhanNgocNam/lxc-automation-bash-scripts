f0f008f0d9b8e011ca6d1848a5345552df05ecef
PhanNgocNam <realcm471@gmail.com>
2025-04-01 18:01:34 +0700

    lxc setup script

---
+++ lxc-setup.sh
@@ -0,0 +1,26 @@
+if [ -z "$1" ]
+then
+    echo "Username is required, ./lxc-setup.sh <username>"
+    exit 1
+fi
+echo "Setting up LXC container for user: $1"
+
+echo "Add user ############################################"
+adduser $1
+
+echo "Change password for user $1 ###########################"
+passwd $1
+
+echo "Grant sudo permissions for $1 #########################"
+cd /etc/sudoers.d && touch $1 && echo "$1 ALL=(ALL) NOPASSWD:ALL" >> $1
+
+echo "Add ssh public key for $1 #############################"
+su - $1
+mkdir -p ~/.ssh
+touch ~/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvHqRmVP5IX7wivyWC1SXlr/cEe3nq3fDDFNHpRrh1P p2n@PhanNgocNams-MacBook-Air.local" >> ~/.ssh/authorized_keys
+
+echo "Install net-tools #####################################"
+apt install -y net-tools >> /dev/null
+
+echo "Done!"
